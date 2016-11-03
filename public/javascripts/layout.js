17c5a4190f5c2a49996fda8fea59d31258167c62
Vaggi <vaganmanuk@gmail.com>
2016-11-03 17:19:16 +0300

    mobile progress

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,18 +1,28 @@
 $(document).ready(function(){
-    
-    $('.menu_icon').click(function(){
-        if ($('.sections_wrapper').hasClass('transformed')) {
-            $('.sections_wrapper').removeClass('transformed');
-            $(this).removeClass('menu_icon_clicked');
-            $('.sections_wrapper').addClass('not_transformed');
-        } else {
-            $('.sections_wrapper').addClass('transformed');
-            $(this).addClass('menu_icon_clicked');
-        }
-    });
-    
-    $('.menu_icon').click(function(){
-        
-    });
+    if($(window).width() > 1024) {    
+        $('.menu_icon').click(function(){
+            if ($('.sections_wrapper').hasClass('transformed')) {
+                $('.sections_wrapper').removeClass('transformed');
+                $(this).removeClass('menu_icon_clicked');
+                $('.sections_wrapper').addClass('not_transformed');
+            } else {
+                $('.sections_wrapper').addClass('transformed');
+                $(this).addClass('menu_icon_clicked');
+            }
+        });
+
+        $('.menu_icon').click(function(){
+
+        });
+    } else 
+        $('.menu_icon').click(function(){
+            if ($('.menu').hasClass('mobile_menu_open')) {
+                $('.menu').removeClass('mobile_menu_open');
+                $(this).removeClass('menu_icon_clicked');
+            } else {
+                $('.menu').addClass('mobile_menu_open');
+                $(this).addClass('menu_icon_clicked');
+            }
+        });
 
 });
